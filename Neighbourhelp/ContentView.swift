17056dacd18419c7d940d951168c01185929c3d3
Julian Vorbrink <julian.vorbrink@stud.hshl.de>
2021-07-01 21:36:26 +0200

    Initial Commit

---
+++ Neighbourhelp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Neighbourhelp
+//
+//  Created by Julian Vorbink on 01.07.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
